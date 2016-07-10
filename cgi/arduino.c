7fd860b600d3d7665a1428b60882688ba2722a56
capybaradino <20378644+capybaradino@users.noreply.github.com>
2016-07-11 00:39:51 +0900

    GET対応他

--- cgi/arduino.c
+++ cgi/arduino.c
@@ -1,52 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <parson.h>
 
 #define CONTENT_LENGTH "CONTENT_LENGTH"
 #define BUF_SIZE 256
 #define REQUEST_METHOD "REQUEST_METHOD"
-#define DAT_FILE_PATH "/home/kame/cgi/data/arduino.dat"
+#define REQUEST_METHOD_GET "GET"
+#define REQUEST_METHOD_POST "POST"
+#define REQUEST_METHOD_PUT "PUT"
+#define REQUEST_METHOD_DELETE "DELETE"
 #define LOG_FILE_PATH "/home/kame/cgi/log/arduino.log"
+#define DAT_FILE_PATH "/home/kame/cgi/data/arduino.dat"
 #define SESSIONID "HTTP_SESSION_ID"
 #include "secret.h"
 
 int error(int);
 //int main( int , char ** );
 void putlog(char *);
+void putdat(const char *);
+void printdat(void);
 
-FILE *fp = NULL;
+FILE *fpLog = NULL;
+FILE *fpDat = NULL;
 
 //int main( int argc , char **argv )
 int main(void)
 {
+	if ((fpLog = fopen(LOG_FILE_PATH, "a")) == NULL) {
+		putlog("log file open error");
+		error(5);
+	}
+//	putlog("main start");
+
+	char* request_method = NULL;
+	if (getenv(REQUEST_METHOD)!=NULL)
+	{
+		request_method = getenv(REQUEST_METHOD);
+	}
+	else
+	{
+		putlog("request method is null");
+		error(8);
+	}
+
+	if (strcmp(request_method, REQUEST_METHOD_GET) != 0)
+	{
+	//Authentication
+	if (getenv(SESSIONID)!=NULL)
+		{
+			char* sessionid = getenv(SESSIONID);
+			if (strcmp(sessionid, SYMMETRICID)==0)
+			{
+				putlog("valid session id");
+			}
+			else
+			{
+	//			putlog("invalid session id");
+				error(6);
+			}
+		}
+		else
+		{
+	//		putlog("session id is null");
+			error(3);
+		}
+	}
+
+	//==================PROCESS FOR GET======================
+	if(strcmp(request_method, REQUEST_METHOD_GET) == 0)
+	{
+//		putlog("GET is called");
+
+  printf( "Content-type: text/html\n\n" );
+
+	printdat();
+
+	}
+	//==================PROCESS FOR POST======================
+	else if(strcmp(request_method, REQUEST_METHOD_POST) == 0)
+	{
+		putlog("POST is called");
+
 	char *pchBuf = NULL;
 	int inLen = 0;
 
   printf( "Content-type: application/json\n\n" );
 
-	if ((fp = fopen(LOG_FILE_PATH, "w")) == NULL) {
-		error(5);
-	}
-	putlog("main start");
 
-	if (getenv(SESSIONID)!=NULL)
-	{
-		char* sessionid = getenv(SESSIONID);
-		if (strcmp(sessionid, SYMMETRICID)==0)
-		{
-			putlog("valid session id");
-		}
-		else
-		{
-			putlog("invalid session id");
-			error(6);
-		}
-	}
-	else
-	{
-		putlog("session id is null");
-		error(3);
-	}
 	
 	if (getenv(CONTENT_LENGTH)!=NULL)
 	{
@@ -87,6 +128,35 @@ int main(void)
 	}
 	putlog("trace 0");
 
+  JSON_Value *root_value = json_parse_string(pchBuf);
+//  JSON_Array *tweets = json_value_get_array(root_value);
+//  for (i = 0; i < json_array_get_count(tweets); i++) {
+//    JSON_Object *tweet = json_array_get_object(tweets, i);
+//    printf("%s: %s\n",
+//      json_object_dotget_string(tweet, "user.screen_name"),
+//      json_object_dotget_string(tweet, "text"));
+//  }
+  JSON_Object *json_object = json_value_get_object(root_value);
+	const char* pchValue = NULL;
+	pchValue = json_object_dotget_string(json_object, "key");
+	pchValue = json_object_dotget_string(json_object, "test");
+
+	if(pchValue != NULL)
+	{
+		putdat(pchValue);
+	}
+
+	JSON_Value *new_root_value = json_value_init_object();
+	JSON_Object *new_json_object = json_value_get_object(new_root_value);
+	json_object_set_string(new_json_object, "hakuna", "matata");
+
+//	printf(json_serialize_to_string(new_root_value));
+
+//	printf("{\"result\":\"%s\"}", pchValue);
+
+  json_value_free(root_value);
+  json_value_free(new_root_value);
+
 #if 0
 	pchBufTemp = pchBuf;
 	for(i=0; i<inLen; i++)
@@ -96,24 +166,41 @@ int main(void)
 		pchBufTemp++;
 	}
 #else
-	printf("%s", pchBuf);
+//	printf("%s", pchBuf);
 #endif
+	free(pchBuf);
 	putlog("trace 1");
 
-	free(pchBuf);
+	putlog("trace end");
+
+	}
+	//==================PROCESS FOR PUT======================
+	else if(strcmp(request_method, REQUEST_METHOD_PUT) == 0)
+	{
+		//TBD
+		putlog("PUT is called");
+	}
+	//==================PROCESS FOR DELETE======================
+	else if(strcmp(request_method, REQUEST_METHOD_DELETE) == 0)
+	{
+		//TBD
+		putlog("DELETE is called");
+	}
+
+
 
 
 
         //printf( "Content-type: text/html\n\n" );
 //        printf( "\n\n" );
 //        printf( "Hello, world.\n" );
-	putlog("trace end");
+	fclose(fpLog);
 	return;
 }
 
 int error(int inErrorCode){
   printf( "{\"error\":\"%d\"}\n", inErrorCode );
-	fclose(fp);
+	fclose(fpLog);
 	exit(EXIT_FAILURE);
 }
 
@@ -128,7 +215,7 @@ void putlog(char* message)
   local = localtime(&timer); /* 地方時に変換 */
 
 	char s[256];
-	sprintf(s, "[%4d/%2d/%2d/%2d:%2d:%2d %d] %s",
+	sprintf(s, "[%4d/%2d/%2d %2d:%2d:%2d %d] %s",
 		local->tm_year + 1900,
 		local->tm_mon + 1,
     local->tm_mday,
@@ -136,7 +223,51 @@ void putlog(char* message)
     local->tm_min,
     local->tm_sec,
     local->tm_isdst, message);
-	fprintf(fp, "%s\n", s);
+	fprintf(fpLog, "%s\n", s);
+	return;
+}
+
+void putdat(const char* message)
+{
+  time_t timer;
+  struct tm *local;
+  struct tm *utc;
+
+  /* 現在時刻を取得 */
+  timer = time(NULL);
+  local = localtime(&timer); /* 地方時に変換 */
+
+	char s[256];
+	if ((fpDat = fopen(DAT_FILE_PATH, "a")) == NULL) {
+		putlog("dat file open error");
+		error(7);
+	}
+#define DATAFORMAT "%d/%d/%d %d:%d:%d\t%s"
+	sprintf(s, DATAFORMAT,
+		local->tm_year + 1900,
+		local->tm_mon + 1,
+    local->tm_mday,
+    local->tm_hour,
+    local->tm_min,
+    local->tm_sec, message);
+	fprintf(fpDat, "%s\n", s);
+	fclose(fpDat);
+	return;
+}
+
+void printdat(void)
+{
+	char s[256];
+	if ((fpDat = fopen(DAT_FILE_PATH, "r")) == NULL) {
+		putlog("dat file open error");
+		error(9);
+	}
+//	while (fscanf(fpDat, "%s", s) != EOF)
+	while (fgets(s, 256, fpDat) != NULL)
+	{
+		printf("%s<br>", s);
+	}
+	fclose(fpDat);
 	return;
 }
 
